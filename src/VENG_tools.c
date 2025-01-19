a7e65f3de2c701648c152d242244284bb9451591
unknown <danielgarciatorras@gmail.com>
2025-01-19 14:17:28 +0100

    Moved Load_IMG wrapper here

---
+++ src/VENG_tools.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+
+SDL_Surface* VENG_LoadPNG (const char* path)
+{
+	SDL_Surface* surface = IMG_Load(path);
+	if (surface == NULL)
+	{
+		printf("Failed to load the PNG image: %s\n", IMG_GetError());
+		return NULL;
+	}
+	return surface;
+}
